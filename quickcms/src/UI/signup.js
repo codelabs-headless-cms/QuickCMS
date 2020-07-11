45b34f73e98de6eb66c10ad6228e0676fca120f9
EC2 Default User <ec2-user@ip-172-31-23-92.us-east-2.compute.internal>
2020-07-11 15:43:39 +0000

    added css files for login and signup pages, updated signup page

--- quickcms/src/UI/signup.js
+++ quickcms/src/UI/signup.js
@@ -6,12 +6,17 @@ class SignUp extends React.Component {
         <div className="loginForm">
           <form className="shadow">
             <div className="form-group">
-              <label for="exampleInputEmail1">Email address</label>
+              <label for="exampleInputEmail1">Enter Your Email Address</label>
               <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
             </div>
             
             <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
+              <label for="exampleInputPassword1">Enter Your Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" />
+            </div>
+            
+            <div class="form-group">
+              <label for="exampleInputPassword1">Confirm Your Password</label>
               <input type="password" class="form-control" id="exampleInputPassword1" />
               <small id="emailHelp" class="form-text text-muted">Already have an account? <a href="login.html"> login here.</a></small>
             </div>
