814d92795ac9cb83bd8085ff75335cbc6bb6a66d
EC2 Default User <ec2-user@ip-172-31-23-92.us-east-2.compute.internal>
2020-07-11 15:26:39 +0000

    base react app installed, UI folder

---
+++ quickcms/src/UI/index.js
@@ -0,0 +1,15 @@
+class MainContainer extends React.Component {
+  
+    render() {
+        return (
+    <div className="container">
+      <h1>Hi</h1>
+    </div>
+    );
+    }
+}
+
+ReactDOM.render(
+  <MainContainer />,
+  document.getElementById('root')
+);
