6a0d1e65b08fcaa66c984da9ad07e707eb2a6cbc
EC2 Default User <ec2-user@ip-172-31-23-92.us-east-2.compute.internal>
2020-07-14 17:47:30 +0000

    created base react app, created sign up and login UI, created client folder and UI folder

---
+++ client/quickcms/src/UI/index.js
@@ -0,0 +1,15 @@
+class MainContainer extends React.Component {
+  
+    render() {
+        return (
+    <div className="container">
+      
+    </div>
+    );
+    }
+}
+
+ReactDOM.render(
+  <MainContainer />,
+  document.getElementById('root')
+);
