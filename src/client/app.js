dab49a53dd5bbb0e2c62c9558c99b5f214158c1c
Yafim Vainilovich <yafimsva@gmail.com>
2020-07-20 04:32:48 -0700

    Create project page, with dynamic form, and its connected to api

--- src/client/app.js
+++ src/client/app.js
@@ -1,31 +1,49 @@
-import React, { Fragment } from "react";
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
+import axios from 'axios';
 
-import Login from "./login/login";
-import SignUp from "./signup/signup";
-// import "./app.css";
+import Login from './login/login';
+import SignUp from './signup/signup';
+import CreateProject from './create-project/create-project';
+import { PrivateRoute } from './components/private-route/private-route';
 
-const App = () => (
-    <Router>
-        <Fragment>
-            <Switch>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/signup">
-                    <SignUp />
-                </Route>
-            </Switch>
-        </Fragment>
-    </Router>
-)
+const App = () => {
+	const [user, setUser] = useState('');
+	useEffect(() => {
+		axios
+			.get('/current_user')
+			.then((result) => setUser(result.data))
+			.catch(() => setUser(false));
+	}, []);
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+	const loginRoutes = [
+		<Route path="/login" component={Login} exact key={1} />,
+		<Route path="/signup" component={SignUp} exact key={2} />,
+		<Redirect to="/login" key={999} />,
+	];
+
+	const userRoutes = [
+		<PrivateRoute
+			path="/new"
+			component={CreateProject}
+			auth={user}
+			exact
+			key={1}
+		/>,
+		<Redirect to="new" key={999} />,
+	];
+
+	return (
+		<Router>
+			<Switch>{user ? userRoutes : loginRoutes}</Switch>
+		</Router>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
