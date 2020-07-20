14d6b90ae4311c0c2b87945329c6f18321b16a28
Yafim Vainilovich <yafimsva@gmail.com>
2020-07-20 04:29:51 -0700

    connected login and sign up to api, form validation

--- src/client/signup/signup.js
+++ src/client/signup/signup.js
@@ -1,41 +1,118 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import axios from 'axios';
+
 import './signup.css';
 import '../app.css';
 
-class SignUp extends React.Component {
+const SignUp = () => {
+	const [user, setUser] = useState({
+		name: '',
+		email: '',
+		password: '',
+		confirmPassword: '',
+	});
+	const [error, setError] = useState('');
+	const [success, setSuccess] = useState(false);
 
-  render() {
-    return (
-      <div className="container">
-        <h4 className="app_name">QuickCMS</h4>
-          <form className="signup-form">
-          <h3 className="heading">Sign Up</h3>
-          <div className="form-group">
-              <label className="form-label" for="name">Your Name</label>
-              <input type="text" class="form-control" placeholder="My project name" />
-            </div>
-            
-            <div className="form-group">
-              <label className="form-label" for="email">Your Email Address</label>
-              <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="myname@email.com"/>
-            </div>
-            
-            <div class="form-group">
-              <label className="form-label" for="InputPassword1">Enter Your Password</label>
-              <input type="password" class="form-control" id="InputPassword1" placeholder="Secret password"/>
-            </div>
-            
-            <div class="form-group">
-              <label className="form-label" for="InputPassword2">Confirm Your Password</label>
-              <input type="password" class="form-control" id="InputPassword2" placeholder="Secret password"/>
-              <small id="emailHelp" class="form-text text-muted">Already have an account? <a href="/login"> login here.</a></small>
-            </div>
-            
-            <button type="submit" class="btn btn-dark">Sign up</button>
-          </form>
-    </div>
-    );
-  }
-}
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (user.password !== user.confirmPassword) {
+			return setError('Passwords do not match');
+		}
+
+		axios
+			.post('/signup', user)
+			.then((response) => {
+				if (response.data.error) {
+					setError(response.data.error);
+				} else {
+					setError(false);
+					setSuccess(true);
+				}
+			})
+			.catch((error) => {
+				setError(true);
+			});
+	};
+
+	if (success) {
+		return <Redirect to="/login" />;
+	}
+
+	return (
+		<div className="container">
+			<h4 className="app_name">QuickCMS</h4>
+			<form className="signup-form form" onSubmit={handleSubmit}>
+				<h3 className="heading">Sign Up</h3>
+				{error ? <span className="text-danger">{error}</span> : <br />}
+				<div className="form-group">
+					<label className="form-label" htmlFor="name">
+						Your Name
+					</label>
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Enter your name here"
+						value={user.name}
+						onChange={(e) => setUser({ ...user, name: e.target.value })}
+					/>
+				</div>
+
+				<div className="form-group">
+					<label className="form-label" htmlFor="email">
+						Your Email Address
+					</label>
+					<input
+						type="email"
+						className="form-control"
+						id="InputEmail"
+						placeholder="myname@email.com"
+						value={user.email}
+						onChange={(e) => setUser({ ...user, email: e.target.value })}
+					/>
+				</div>
+
+				<div className="form-group">
+					<label className="form-label" htmlFor="InputPassword1">
+						Enter Your Password
+					</label>
+					<input
+						type="password"
+						className="form-control"
+						id="InputPassword1"
+						placeholder="Secret password"
+						value={user.password}
+						onChange={(e) => setUser({ ...user, password: e.target.value })}
+					/>
+				</div>
+
+				<div className="form-group">
+					<label className="form-label" htmlFor="InputPassword2">
+						Confirm Your Password
+					</label>
+					<input
+						type="password"
+						className="form-control"
+						id="InputPassword2"
+						placeholder="Secret password"
+						value={user.confirmPassword}
+						onChange={(e) =>
+							setUser({ ...user, confirmPassword: e.target.value })
+						}
+					/>
+					<small id="emailHelp" className="form-text text-muted">
+						Already have an account? <Link to="/login"> login here.</Link>
+					</small>
+				</div>
+
+				<button type="submit" className="btn btn-dark">
+					Sign up
+				</button>
+			</form>
+		</div>
+	);
+};
 
 export default SignUp;
